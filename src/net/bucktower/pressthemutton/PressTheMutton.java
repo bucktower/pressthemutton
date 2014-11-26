da65d7fdbe03cf985eac27c49f98f3306be83dcd
Buck <bucktower14@gmail.com>
2014-11-26 17:27:01 -0600

    added other images - grass and originals

--- src/net/bucktower/pressthemutton/PressTheMutton.java
+++ src/net/bucktower/pressthemutton/PressTheMutton.java
@@ -5,12 +5,14 @@ import processing.core.PApplet;
 
 public class PressTheMutton extends PApplet {
 
-	int cataMargin;
-	int cataHeight;
-	int cataWidth;
+	int cataMargin = 150;
+	int cataHeight = 200;
+	int cataWidth = 30;
 	
 	public void setup() {
-		background(0);
+		noStroke();
+		
+		background(45,150,169);
 		size(1280,600);
 	}
 
@@ -19,7 +21,8 @@ public class PressTheMutton extends PApplet {
 	}
 	
 	private void refreshCata(){
-		
+		fill(130,60,10);
+		rect(cataMargin,height-cataHeight,cataWidth,cataHeight);
 	}
 	
 	public static void main(String _args[]) {
