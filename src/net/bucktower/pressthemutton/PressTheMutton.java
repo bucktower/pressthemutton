63ef2da72981a0b5cc82297eb7ab3fac46e070ec
Buck <bucktower14@gmail.com>
2014-11-26 01:37:46 -0600

    initial commit

---
+++ src/net/bucktower/pressthemutton/PressTheMutton.java
@@ -0,0 +1,17 @@
+package net.bucktower.pressthemutton;
+
+import processing.core.PApplet;
+
+
+public class PressTheMutton extends PApplet {
+
+	public void setup() {
+	}
+
+	public void draw() {
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { net.bucktower.pressthemutton.PressTheMutton.class.getName() });
+	}
+}
