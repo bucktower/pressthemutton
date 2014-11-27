17280fbe7a3e9d85c2e7340ca3ee3590967d2c4f
Buck <bucktower14@gmail.com>
2014-11-27 16:21:04 -0600

    working aiming w/ drawing mutton & string, random grass

--- src/net/bucktower/pressthemutton/PressTheMutton.java
+++ src/net/bucktower/pressthemutton/PressTheMutton.java
@@ -5,24 +5,83 @@ import processing.core.PApplet;
 
 public class PressTheMutton extends PApplet {
 
+	//catapult
+	ImageEditor cata;
 	int cataMargin = 150;
-	int cataHeight = 200;
-	int cataWidth = 30;
+		
+	//grass
+	ImageEditor grass;
+	int grass1X;
+	int grass2X;
+	int grass3X;
 	
+	//sheep
+	ImageEditor sheep;
+
 	public void setup() {
+		SketchObject.setApp(this);	
+
+		grass = new ImageEditor("net/bucktower/pressthemutton/data/grasshd.png");
+		cata = new ImageEditor("net/bucktower/pressthemutton/data/catahd.png");
+		sheep = new ImageEditor("net/bucktower/pressthemutton/data/muttonhd.png");
+
 		noStroke();
 		
-		background(45,150,169);
 		size(1280,600);
+		
+		//grass
+		grass1X = (int)random(cataMargin+cata.width(),width-grass.width());
+		grass2X = (int)random(cataMargin+cata.width(),width-grass.width());
+		grass3X = (int)random(cataMargin+cata.width(),width-grass.width());
+		
+		drawGame(false);
 	}
 
 	public void draw() {
-		refreshCata();
 	}
 	
-	private void refreshCata(){
-		fill(130,60,10);
-		rect(cataMargin,height-cataHeight,cataWidth,cataHeight);
+	public void mousePressed(){
+		if(mouseX < cataMargin+cata.width() && mouseY > height-cata.height()){
+			drawGame(true);
+		}
+	}
+	
+	public void mouseDragged(){
+		if(mouseX < cataMargin+cata.width() && mouseY > height-cata.height()){
+			drawGame(true);
+		}
+	}
+	
+	private void drawGame(boolean drawSheep){
+		background(45,150,169);
+		drawGrass();
+		drawCata();
+		
+		if(drawSheep){
+			drawString();
+			drawSheep();
+		}
+	}
+	
+	private void drawCata(){
+		cata.drawAt(cataMargin, height-cata.height());
+	}
+	
+	private void drawGrass(){
+		grass.drawAt(grass1X,height-grass.height());
+		grass.drawAt(grass2X,height-grass.height());
+		grass.drawAt(grass3X,height-grass.height());
+	}
+	
+	private void drawSheep(){
+		sheep.drawAt(mouseX-sheep.width()/2, mouseY-sheep.height()/2);
+	}
+	
+	private void drawString(){
+		stroke(255);
+		line(cataMargin, height-cata.height(), mouseX, mouseY);
+		line(cataMargin+cata.width(), height-cata.height(), mouseX, mouseY);
+		noStroke();
 	}
 	
 	public static void main(String _args[]) {
